1c9abe6ccd1790c39248154509e6c6b82a71fc1d
Emily Vit�ria de Souza Ramos <42749126800@>
2025-01-27 13:03:55 -0300

    Salvar alterações locais antes do merge

--- 23.01/POO5.py
+++ 23.01/POO5.py
@@ -1,1 +1,8 @@
 # 5.	Crie uma classe chamada “Funcionário” com atributos para armazenar o nome, o salário e o cargo do funcionário. Implemente métodos para calcular o salário líquido, considerando descontos de impostos e benefícios.
+
+class Funcionario():
+    def __init__(self, nome, salario, cargo):
+        self.nome = nome
+        self.salario = salario
+        self.cargo = cargo
+        
