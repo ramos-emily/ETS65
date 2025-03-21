e101dd7b868f0fe836b6b7bafaf9b2ba807df664
ramos-emily <ramos.emilyvi@gmail.com>
2025-03-21 14:54:56 -0300

    dorivas ajudou

--- PBE/20.03/crud/projeto/app/serializers.py
+++ PBE/20.03/crud/projeto/app/serializers.py
@@ -1,9 +1,3 @@
 from rest_framework import serializers
 from .models import Dados
 
-class appSerializer(serializers.ModelSerializer):
-    is_active = serializers.BooleanField(read_only=True)
-
-    class meta:
-        model = Dados
-        fields = ('')
--- PBE/20.03/crud/projeto/app/views.py
+++ PBE/20.03/crud/projeto/app/views.py
@@ -1,6 +1,5 @@
-from django.shortcuts import render
+from rest_framework.response import Response
 from .models import *
-
 
 
 
