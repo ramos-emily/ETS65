ea9c4f1d4df87f1fc86d65d199c6a5240eacdc00
ramos-emily <ramos.emilyvi@gmail.com>
2025-04-10 16:57:32 -0300

    DjangoDeGente

--- PBE/classesBased/django/pato/views.py
+++ PBE/classesBased/django/pato/views.py
@@ -3,12 +3,12 @@ from rest_framework.generics import ListCreateAPIView
 from .models import Pato
 from .serializers import PatoSerializer
 from rest_framework.pagination import PageNumberPagination
+from rest_framework import serializers
 
 class PatoPaginacao(PageNumberPagination):
     page_size = 3
     page_size_query_param = 'page_size'
     max_page_size = 10
-
 
 class PatoListCreateAPIView(ListCreateAPIView):
     queryset = Pato.objects.all()
@@ -21,4 +21,8 @@ class PatoListCreateAPIView(ListCreateAPIView):
         if nome:
             queryset = queryset.filter(nome__incontains=nome)
         return queryset
+    def perform_create(self, serializer):
+        if serializer.validated_data['peso'] < 0:
+            raise serializers.ValidationError("O peso nao pode ser nagitvo")
+        serializer.save()
     
