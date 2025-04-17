9b110309e28356a1acda340ebea7202d8893786c
ramos-emily <ramos.emilyvi@gmail.com>
2025-04-17 16:20:49 -0300

    crud

---
+++ PBE/Atividade17.04/app/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class Usuario(models.Model):
+    bio = models.CharField( max_length=50)
+    idade = models
+    tell = models
+    endereco = models
+    escola = models
+    animais = models
