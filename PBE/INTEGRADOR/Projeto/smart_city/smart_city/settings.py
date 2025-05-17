02c8135ed1f165b6e3c03c21de0512c86f78260a
ramos-emily <ramos.emilyvi@gmail.com>
2025-05-17 16:21:09 -0300

    ajustando settings

--- PBE/INTEGRADOR/Projeto/smart_city/smart_city/settings.py
+++ PBE/INTEGRADOR/Projeto/smart_city/smart_city/settings.py
@@ -40,8 +40,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'rest_framework_simplejwt',
+    'drf_yasg', 
     'api_smart',
-    'rest_framework_swagger',
+
 ]
 
 MIDDLEWARE = [
