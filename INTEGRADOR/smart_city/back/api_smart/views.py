ea918066f12244c1213899138913a8da1fa0b26c
ramos-emily <ramos.emilyvi@gmail.com>
2025-05-25 19:43:41 -0300

    IsAuthenticated

--- INTEGRADOR/smart_city/back/api_smart/views.py
+++ INTEGRADOR/smart_city/back/api_smart/views.py
@@ -1,8 +1,7 @@
-from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView, ListAPIView
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from .models import Sensor, Ambientes, Historico
 from .serializers import SensorSerializer, AmbientesSerializer, HistoricoSerializer
-from .permissions import IsGestor
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import filters, status
 import pandas as pd
@@ -11,6 +10,38 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from django.contrib.auth.models import User
+from rest_framework.filters import SearchFilter
+
+class AmbientesListCreateAPIView(ListCreateAPIView):
+    queryset = Ambientes.objects.all()
+    serializer_class = AmbientesSerializer
+    permission_classes = [IsAuthenticated]
+
+class AmbientesDetailAPIView(RetrieveUpdateDestroyAPIView):
+    queryset = Ambientes.objects.all()
+    serializer_class = AmbientesSerializer
+    permission_classes = [IsAuthenticated]
+
+class AmbientesSearchAPIView(ListAPIView):
+    queryset = Ambientes.objects.all()
+    serializer_class = AmbientesSerializer
+    permission_classes = [IsAuthenticated]
+    filter_backends = (DjangoFilterBackend, SearchFilter)
+    search_fields = ['id', 'sig', 'descricao', 'ni', 'responsavel']
+
+@api_view(['GET', 'POST'])
+@permission_classes([IsAuthenticated])
+def listar_ambientes(request):
+    if request.method == 'GET':
+        queryset = Ambientes.objects.all()
+        serializer = AmbientesSerializer(queryset, many=True)
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = AmbientesSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 @api_view(['GET'])
 @permission_classes([IsAuthenticated])
@@ -30,13 +61,10 @@ class RegisterAPIView(APIView):
         username = request.data.get('username')
         email = request.data.get('email')
         password = request.data.get('password')
-        
         if not username or not email or not password:
             return Response({'error': 'Todos os campos são obrigatórios'}, status=status.HTTP_400_BAD_REQUEST)
-            
         if User.objects.filter(username=username).exists():
             return Response({'error': 'Nome de usuário já existe'}, status=status.HTTP_400_BAD_REQUEST)
-            
         user = User.objects.create_user(username=username, email=email, password=password)
         return Response({'success': 'Usuário criado com sucesso'}, status=status.HTTP_201_CREATED)
 
@@ -46,42 +74,19 @@ class SensorListCreateAPIView(ListCreateAPIView):
     filter_backends = [DjangoFilterBackend, filters.SearchFilter]
     filterset_fields = ['status', 'sensor']
     search_fields = ['mac_address']
-
-    def get_permissions(self):
-        if self.request.method == 'GET':
-            return [IsAuthenticated()]
-        return [IsGestor()]
+    permission_classes = [IsAuthenticated]
 
 class SensorDetailAPIView(RetrieveUpdateDestroyAPIView):
     queryset = Sensor.objects.all()
     serializer_class = SensorSerializer
-    def get_permissions(self):
-        return [IsGestor()]
-
-class AmbientesListCreateAPIView(ListCreateAPIView):
-    queryset = Ambientes.objects.all()
-    serializer_class = AmbientesSerializer
-    def get_permissions(self):
-        if self.request.method == 'GET':
-            return [IsAuthenticated()]
-        return [IsGestor()]
-
-class AmbientesDetailAPIView(RetrieveUpdateDestroyAPIView):
-    queryset = Ambientes.objects.all()
-    serializer_class = AmbientesSerializer
-    def get_permissions(self):
-        return [IsGestor()]
+    permission_classes = [IsAuthenticated]
 
 class HistoricoListCreateAPIView(ListCreateAPIView):
     queryset = Historico.objects.all()
     serializer_class = HistoricoSerializer
-    def get_permissions(self):
-        if self.request.method == 'GET':
-            return [IsAuthenticated()]
-        return [IsGestor()]
+    permission_classes = [IsAuthenticated]
 
 class HistoricoDetailAPIView(RetrieveUpdateDestroyAPIView):
     queryset = Historico.objects.all()
     serializer_class = HistoricoSerializer
-    def get_permissions(self):
-        return [IsGestor()]
+    permission_classes = [IsAuthenticated]
