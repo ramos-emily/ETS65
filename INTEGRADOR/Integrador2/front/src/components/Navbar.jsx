3340157bfd4f7f20d4876254b3c8d0b38bd0d258
ramos-emily <ramos.emilyvi@gmail.com>
2025-06-02 16:56:56 -0300

    infoBar estilizada

--- INTEGRADOR/Integrador2/front/src/components/Navbar.jsx
+++ INTEGRADOR/Integrador2/front/src/components/Navbar.jsx
@@ -5,79 +5,85 @@ import history from "../assets/history.svg";
 import location from "../assets/location.svg";
 import logout from "../assets/logout.svg";
 import sensors from "../assets/sensors.svg";
-import search from "../assets/search.svg"
-
+import search from "../assets/search.svg";
 
 
 export function Navbar() {
-
   const [searchTerm, setSearchTerm] = useState("");
-  
+
   let linkNav = [
     {
       img: home,
       link: "/home",
-      altImg: "Icone para localizar a página Home",
-      titulo: "Home"
+      altImg: "Ícone para localizar a página Home",
+      titulo: "Home",
     },
     {
       img: location,
       link: "/ambiente",
-      altImg: "Icone para localizar a página de Ambientes",
-      titulo: "Ambientes"
+      altImg: "Ícone para localizar a página de Ambientes",
+      titulo: "Ambientes",
     },
     {
       img: sensors,
       link: "/sensores",
-      altImg: "Icone para localizar a página de Sensores",
-      titulo: "Sensores"
+      altImg: "Ícone para localizar a página de Sensores",
+      titulo: "Sensores",
     },
     {
       img: history,
       link: "/historico",
-      altImg: "Icone para localizar a página de Histórico",
-      titulo: "Histórico"
-    }
-    
+      altImg: "Ícone para localizar a página de Histórico",
+      titulo: "Histórico",
+    },
   ];
 
   return (
     <nav className="text-[#226D13] bg-white shadow-bottom w-full fixed top-0 z-10 h-20">
-    <div className="container mx-auto flex items-center h-full px-8">
-      {/* Título alinhado à esquerda (não muda) */}
-    <div className="text-2xl font-bold mr-auto">Smart City</div> {/* mr-auto empurra o resto para a direita */}
-    <div className="flex items-center bg-white shadow-md rounded w-[67%] sm:w-[60%] lg:w-[65%] h-12 lg:h-14 ">
-        <img src={search} alt="Ícone da barra de pesquisa" className="w-5 h-5 !ml-5" />
-        <input
-            type="text" 
-            placeholder="Buscar..." 
-            className="!ml-2 w-full outline-none text-sm "
-            value={searchTerm} 
-            onChange={(e) => setSearchTerm(e.target.value)}
-        />
-    </div>
-        
-    {/* Conteúdo alinhado à direita */}
-    <div className="flex justify-end flex-1"> {/* flex-1 + justify-end alinha tudo à direita */}
-      <ul className="flex gap-6 items-center h-full">
-        {linkNav.map((navRender, index) => (
-          <Link key={index} to={navRender.link}>
-            <li className="flex items-center gap-2 hover:shadow-lg hover:scale-105 transition-all px-4 py-2 rounded-md cursor-pointer h-full">
-              <img src={navRender.img} alt={navRender.altImg} className="w-6 h-6"/>
-              <p className="text-[17px] font-medium">{navRender.titulo}</p>
+      <div className="mx-auto flex items-center justify-center h-full gap-150">
+        <div className="flex items-center gap-4 ">
+          <div className="flex items-center shadow-md rounded w-100 h-10 ml-4 bg-[#faf9f9] gap-2">
+            <img
+              src={search}
+              alt="Ícone de pesquisa"
+              className="w-4 h-4 ml-3 "
+            />
+            <input
+              type="text"
+              placeholder="Buscar..."
+              className="ml-2 w-full outline-none text-sm "
+              value={searchTerm}
+              onChange={(e) => setSearchTerm(e.target.value)}
+            />
+          </div>
+        </div>
+
+        <ul className="flex gap-10 items-center">
+          {linkNav.map((navRender, index) => (
+            <Link key={index} to={navRender.link}>
+              <li className="flex items-center gap-2 hover:shadow-lg hover:scale-105 transition-all px-4 py-2 rounded-md cursor-pointer">
+                <img
+                  src={navRender.img}
+                  alt={navRender.altImg}
+                  className="w-6 h-6"
+                />
+                <p className="text-lg font-semibold">{navRender.titulo}</p>
+              </li>
+            </Link>
+          ))}
+
+          <Link to="/" onClick={() => localStorage.removeItem("token")}>
+            <li className="flex items-center gap-2 hover:shadow-lg hover:scale-105 transition-all px-4 py-2 rounded-md cursor-pointer">
+              <img
+                src={logout}
+                alt="Ícone de sair"
+                className="w-6 h-6"
+              />
+              <p className="text-lg font-semibold">Sair</p>
             </li>
           </Link>
-        ))}
-
-        <Link to="/" onClick={() => localStorage.removeItem("token")}>
-          <li className="flex items-center gap-2 hover:shadow-lg hover:scale-105 transition-all px-4 py-2 rounded-md cursor-pointer">
-            <img src={logout} alt="Icone para localizar a saída da plataforma" className="w-6 h-6"/>
-            <p className="text-[17px] font-medium">Sair</p>
-          </li>
-        </Link>
-      </ul>
-    </div>
-  </div>
-</nav>
+        </ul>
+      </div>
+    </nav>
   );
 }
