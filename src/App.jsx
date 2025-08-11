25eec34cbb72de0d8ded5edf559c6adcefdda97e
ramos-emily <ramos.emilyvi@gmail.com>
2025-08-10 21:17:23 -0300

    mudança de window

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./Pages/home";
-import { useEffect } from "react";
 
 export default function App() {
-  useEffect(() => {
-    window.enviarResultado = function (name, score, time) {
-      console.log("Função window.enviarResultado chamada com:", {
-        name,
-        score,
-        time,
-      });
-
-      fetch("https://api-ets.vercel.app/api/ranking", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ name, score, time }),
-      })
-        .then((res) => {
-          console.log("Resposta do servidor:", res.status);
-          if (!res.ok) throw new Error("Erro ao enviar resultado");
-          return res.json();
-        })
-        .then((data) => {
-          console.log("Resultado enviado com sucesso:", data);
-        })
-        .catch((err) => {
-          console.error("Falha no envio:", err);
-        });
-    };
-  }, []);
-
   return (
     <Routes>
       <Route path="/" element={<Home />} />
